0527c14ca95188c00a1ccfb06d462ed026b9a113
Jose Gutierro <jose.gutierro@estudiocactus.com>
2025-07-16 19:25:19 +0200

    ♻️  replace custom icon implementation with lucide dynamic import

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,16 +1,18 @@
-import { icons, LucideProps } from "lucide-react";
+// import { icons, LucideProps } from "lucide-react";
 
-interface IconProps extends LucideProps {
-  name: keyof typeof icons;
-}
+// interface IconProps extends LucideProps {
+//   name: keyof typeof icons;
+// }
 
-export const Icon = ({ name, ...props }: IconProps) => {
-  const IconComponent = icons[name as keyof typeof icons];
+// export const Icon = ({ name, ...props }: IconProps) => {
+//   const IconComponent = icons[name as keyof typeof icons];
 
-  if (!IconComponent) {
-    console.error(`LucideIcon: No icon found for the name '${name}'`);
-    return null;
-  }
+//   if (!IconComponent) {
+//     console.error(`LucideIcon: No icon found for the name '${name}'`);
+//     return null;
+//   }
 
-  return <IconComponent {...props} />;
-};
+//   return <IconComponent {...props} />;
+// };
+
+export { DynamicIcon as Icon } from "lucide-react/dynamic";
